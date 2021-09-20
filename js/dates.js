8f741b7d462c642c5ade82bf6eccf4016c841930
cla21018@byui.edu <cla21018@byui.edu>
2021-09-20 18:40:56 +0200

    week 1

--- js/dates.js
+++ js/dates.js
@@ -1,3 +1,9 @@
+//const copyrightyear = document.getElementById("copyrightyear");
+//copyrightyear.textContent = new Date().getFullYear();
+
+//there is a nother better way to select elemnts ftom another file is the step below)
+
+
 document.getElementById("copyrightyear").textContent = new Date().getFullYear();
 
-"document.querySelector("#"copyrightyear")."textContent = new Date().getFullYear();;"
+//document.querySelector("#copyrightyear").textContent = new Date().getFullYear();
